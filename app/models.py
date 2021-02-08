9b980a8e1e80a2f0edfe9e9e8dd1af7eaf683174
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2021-02-08 00:26:43 -0500

    Almost done setting up GET request without loading the CSV on my repo

--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from adaptor.model import CsvModel
 
 # Create your models here.
+class ipv4 (models.Model):
+    network = DecimalField()
+    geoname_id = IgnoredField()
+    resgistered_country_geoname_id = IgnoredField()
+    represented_country_geoname_id = IgnoredField()
+    is_anonymous_proxy = IgnoredField()
+    is_satellite_provider = IgnoredField()
+    postal_code = IgnoredField()
+    latitude = DecimalField()
+    longitude = DecimalField()
+    accuracy_radius = IgnoredField()
+
+class MycsvModel (CsvModel):
+    network = IgnoredField()
+    geoname_id = IgnoredField()
+    resgistered_country_geoname_id = IgnoredField()
+    represented_country_geoname_id = IgnoredField()
+    is_anonymous_proxy = IgnoredField()
+    is_satellite_provider = IgnoredField()
+    postal_code = IgnoredField()
+    latitude = DecimalField()
+    longitude = DecimalField()
+    accuracy_radius = IgnoredField()
+
+    def __str__(self):
+        return self.name
